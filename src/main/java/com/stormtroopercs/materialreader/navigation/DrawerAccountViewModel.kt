18cceee358dd47818ae3a75297992abaa3edff4d
stormtroopercs <1967616+stormtroopercs@users.noreply.github.com>
2026-09-05 06:31:36 -0700

    Fix Checkstyle header violations in the two new navigation files
    Cause: CI's Checkstyle task (maxWarnings = 0, byte-exact GPL header
    match) failed with 2 errors: CommunityIconFallback.kt had a
    re-wrapped header, DrawerAccountViewModel.kt said "version 3.0"
    instead of "version 3". The release build had already been fixed by
    the previous commit, so this was the next gate v0.0.20-alpha hit.
    
    Fix: restore the exact config/checkstyle/copyright.java.txt header in
    both files. Verified: ./gradlew Checkstyle passes.

--- src/main/java/com/stormtroopercs/materialreader/navigation/DrawerAccountViewModel.kt
+++ src/main/java/com/stormtroopercs/materialreader/navigation/DrawerAccountViewModel.kt
@@ -3,7 +3,7 @@
  *
  * MaterialReader is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3.0, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * MaterialReader is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MaterialReader.  If not, see <http:></http://www.gnu.org/licenses/>.
+ * along with MaterialReader.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 package com.stormtroopercs.materialreader.navigation
